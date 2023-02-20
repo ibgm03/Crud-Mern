28bc0e76b074d449c8510b473bf8843c13557a0e
Benjamin Glez <ibgm03@gmail.com>
2023-02-20 14:51:16 -0600

    COnfiguración de server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res) => {
+    res.end('Bienvenidos al servidor backend Node.js. Corriendo...<')
+})
+
+//configurar server basico
+app.listen(5000, function () {
+    console.log("El servidor NODE esta corriendo correctamente")
+})
