2f7ad1220c2cbf7ded86340d2a47bbce0b401338
Benjamin Glez <ibgm03@gmail.com>
2023-02-28 09:08:45 -0600

    add backend

---
+++ rutas/usuario.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const eschema = mongoose.Schema
+
+const eschemausuario = new eschema({
+    nombre: String,
+    email: String,
+    telefono: String,
+    idusuario: String
+})
